400a738696b61e55f2959c418428b4d8b93896a2
Alex Smith <alexsmith1095@gmail.com>
2021-06-09 15:17:19 +0100

    Setup tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import 'react-notion/src/styles.css'
 import 'prismjs/themes/prism-tomorrow.css'
+import 'tailwindcss/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />
